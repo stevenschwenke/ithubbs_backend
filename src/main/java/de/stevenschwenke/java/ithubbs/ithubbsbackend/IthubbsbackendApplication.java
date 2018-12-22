55716703c0a7f49db97e3f9fccd7658c6d2b7a24
Steven Schwenke <Steven.Schwenke@msg-david.de>
2018-12-22 13:37:46 +0100

    Initial commit

---
+++ src/main/java/de/stevenschwenke/java/ithubbs/ithubbsbackend/IthubbsbackendApplication.java
@@ -0,0 +1,14 @@
+package de.stevenschwenke.java.ithubbs.ithubbsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthubbsbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IthubbsbackendApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/de/stevenschwenke/java/ithubbs/ithubbsbackend/IthubbsbackendApplicationTests.java
@@ -0,0 +1,17 @@
+package de.stevenschwenke.java.ithubbs.ithubbsbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IthubbsbackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
