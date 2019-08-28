10b188201732c1b7eedf6d00d8b5f70c2b760870
Steven Schwenke <Steven.Schwenke@msg-david.de>
2019-08-28 08:53:09 +0200

    Add insert-statement for default database user for test

--- src/test/java/de/stevenschwenke/java/ithubbs/ithubbsbackend/authentication/DomainUserDetailsServiceTest.java
+++ src/test/java/de/stevenschwenke/java/ithubbs/ithubbsbackend/authentication/DomainUserDetailsServiceTest.java
@@ -10,5 +10,6 @@ class DomainUserDetailsServiceTest {
     void encodeMe() {
         System.out.println(new BCryptPasswordEncoder().encode("steven"));
         // $2a$10$UoSI1GfEiU1MTsqd/NL2Gur2nKU7Yrvxsce0GohoNiv91NcJvNCea
+        // insert into users(id, username, password) values (1, 'steven', '$2a$10$UoSI1GfEiU1MTsqd/NL2Gur2nKU7Yrvxsce0GohoNiv91NcJvNCea');
     }
 }
