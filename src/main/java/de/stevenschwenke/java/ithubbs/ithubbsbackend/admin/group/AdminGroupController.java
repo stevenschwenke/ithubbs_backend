3ff2077c6e2a1c317a982387ae3bd0f35ae0183b
Steven Schwenke <steven@stevenschwenke.de>
2019-11-30 16:12:13 +0100

    Add endpoint to download group logos

--- src/main/java/de/stevenschwenke/java/ithubbs/ithubbsbackend/admin/group/AdminGroupController.java
+++ src/main/java/de/stevenschwenke/java/ithubbs/ithubbsbackend/admin/group/AdminGroupController.java
@@ -1,7 +1,12 @@
 package de.stevenschwenke.java.ithubbs.ithubbsbackend.admin.group;
 
 import de.stevenschwenke.java.ithubbs.ithubbsbackend.group.Group;
+import de.stevenschwenke.java.ithubbs.ithubbsbackend.group.GroupLogo;
+import de.stevenschwenke.java.ithubbs.ithubbsbackend.group.GroupLogoRepository;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,18 +21,34 @@ public class AdminGroupController {
 
     private final AdminGroupRepository adminGroupRepository;
     private final AdminGroupService adminGroupService;
+    private final GroupLogoRepository groupLogoRepository;
 
     @Autowired
     public AdminGroupController(AdminGroupRepository adminGroupRepository,
-                                AdminGroupService adminGroupService) {
+                                AdminGroupService adminGroupService,
+                                GroupLogoRepository groupLogoRepository) {
         this.adminGroupRepository = adminGroupRepository;
         this.adminGroupService = adminGroupService;
+        this.groupLogoRepository = groupLogoRepository;
     }
 
     @GetMapping(value = "")
     public ResponseEntity<List<Group>> getAllGroup() {
 
         return new ResponseEntity<>(adminGroupRepository.findAll(), HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/logo/{logoId}")
+    public ResponseEntity<byte[]> handler(@PathVariable("logoId") long logoId) {
+
+        GroupLogo groupLogo = this.groupLogoRepository.findById(logoId).orElseThrow();
+        byte[] content = ArrayUtils.toPrimitive(groupLogo.getContent());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+
+        return new ResponseEntity<>(content, headers, HttpStatus.OK);
     }
 
     @PostMapping(value = "")
